9db4fd767e596deafce53e319481ebb19ace2c31
HKMANOJ <hkmanoj66@gmail.com>
2023-01-07 22:17:47 +0530

    "newco"

--- src/queue/ExampleQueue.java
+++ src/queue/ExampleQueue.java
@@ -9,13 +9,31 @@ public class ExampleQueue {
         sd.offer(23);
         sd.offer(32);
         sd.offer(323);
-        System.out.println(sd);
+        System.out.print(sd);
+        System.out.println();
+        while (!sd.isEmpty()){
+            System.out.print(sd.remove()+" ");
+        }
         Deque<Integer> sd1=new ArrayDeque<>();
         sd1.offer(32);
         sd1.offer(2);
         sd1.offer(3);
         sd1.offer(321);
-        System.out.println(sd1);
-
+        System.out.print(sd1);
+        System.out.println();
+        while (!sd1.isEmpty()){
+            System.out.print(sd1.remove()+" ");
+        }
+        PriorityQueue<Integer>sdd=new PriorityQueue<>();
+        sdd.add(23);
+        sdd.add(32);
+        sdd.add(44);
+        sdd.add(11);
+        sdd.add(22);
+        System.out.print(sdd);
+        System.out.println();
+        while (!sdd.isEmpty()){
+            System.out.print(sdd.remove()+" ");
+        }
     }
 }
