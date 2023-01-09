776562312a0f084973e25f28fc2d95062a34c86d
HKMANOJ <hkmanoj66@gmail.com>
2023-01-09 21:31:24 +0530

    "newco"

---
+++ src/Tree/ExampleTree.java
@@ -0,0 +1,39 @@
+package Tree;
+
+import java.util.Scanner;
+
+public class ExampleTree {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    createTree();
+    }
+
+    static node createTree() {
+        node root = null;
+        System.out.println("Enter data");
+        int data = sc.nextInt();
+        if (data == -1) return null;
+        root = new node(data);
+        System.out.println("enter data left");
+        root.left = createTree();
+        return root;
+    }
+}
+   class node{
+        node left,right;
+        int data;
+
+        public node(int data) {
+            this.data=data;
+        }
+    }
+
+
+//    static void inOederTreversal(node root){
+//        inOederTreversal(root.left);
+//        System.out.println(root.data);
+//        inOederTreversal(root.left);
+//    }
+
+
