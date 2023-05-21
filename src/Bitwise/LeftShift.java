408971e515cb582caa5fc2af716bbb9b6a73ae56
HKMANOJ <hkmanoj66@gmail.com>
2023-05-21 23:01:15 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Bitwise/LeftShift.java
@@ -0,0 +1,11 @@
+package Bitwise;
+
+public class LeftShift {
+    public static void main(String[] args) {
+        int n=2;//0010<<2 shifting 2 time left side=1000 in decimal ans =8
+        System.out.println(n<<2);
+
+        int m=3;//0011<<2 shifting 2 time left side=1100 in decimal ans=12
+        System.out.println(m<<2);
+    }
+}
